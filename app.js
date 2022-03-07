b9cf2f75e09f901016ca27a7968aeb473af9c266
mayara <luziamayara83@gmail.com>
2022-03-07 15:34:25 -0300

    criação de uma fatch

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const fetchPokemon = () =>{
-    const url = 'https://pokeapi.co/api/v2/pokemon/'
-    fech(url)
+    const gePokemonurl = id => 'https://pokeapi.co/api/v2/pokemon/${id}'  
+    const pokemonPromises = []
+    
+    for (let i = 1; i <= 150; i++){
+        pokemonPromises.push()
+        fech(getPokemonurl(i)).then(response => response.json()) 
+    }
+    /*  °função que busca pokemon, fuch é uma requisição em javascrip 
+        °
+    */
 }
  
-fetchPokemon()
+fetchPokemon() 
