2d944bbba7158701936cdb61be6df82f12fb2725
mayara <luziamayara83@gmail.com>
2022-03-07 16:02:21 -0300

    reduzir array

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const fetchPokemon = () =>{
-    const gePokemonurl = id => 'https://pokeapi.co/api/v2/pokemon/${id}'  
+    const getPokemonurl = id => 'https://pokeapi.co/api/v2/pokemon/${id}' 
+
     const pokemonPromises = []
     
     for (let i = 1; i <= 150; i++){
-        pokemonPromises.push()
-        fech(getPokemonurl(i)).then(response => response.json()) 
+    
+        pokemonPromises.push(fech(getPokemonurl(i)).then(response => response.json()) )
     }
     /*  °função que busca pokemon, fuch é uma requisição em javascrip 
-        °
+        °metodo estatico
+        °reduzir o array em uma string ( reduce)
     */
+
+        Promise.all(pokemonPromises)
+        .then(pokemons => {
+            console.log(pokemons)
+
+            const listpomons = pokemons.reduce()
+        })
+
 }
  
 fetchPokemon() 
